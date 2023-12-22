c655ceba443fc80ed81dd7fb519fc721e092d5cf
kingsalah94 <mohahamedbahachimis@gmail.com>
2023-12-23 00:09:21 +0100

    Implementation de la page Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageLoginComponent } from './Pages/page-login/page-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'login',
+    component: PageLoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
